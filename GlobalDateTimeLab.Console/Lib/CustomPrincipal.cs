c4875565218099d6884e656d677b78926c6adee8
kim <kim@bud4.net>
2019-02-12 17:19:07 +0800

    remove redauntant namespace

--- GlobalDateTimeLab.Console/Lib/CustomPrincipal.cs
+++ GlobalDateTimeLab.Console/Lib/CustomPrincipal.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Security;
-
 namespace GlobalDateTimeLab.Console.Lib
 {
     public class CustomPrincipal : GenericPrincipal
